0c1063665c83083ab3e426a3d969ba5fbe7f64dd
LAVINSE <freedomsewon@gmail.com>
2023-12-09 19:53:11 +0900

    study
    study

--- BaekJoon/BaekJoon10810/Program.cs
+++ BaekJoon/BaekJoon10810/Program.cs
@@ -5,6 +5,46 @@ public class Program
 {
     private static void Main(string[] args)
     {
+        /*
+         * Ex) i =1, j =2, k =3
+         * - 3 3 0 0 0
+         * >> i =3, j =4, k =4 
+         * - 3 3 4 4 0
+         * >> i =1, j =4, k =1
+         * - 1 1 1 1 0
+         * >> i =2 , j =2, k =2
+         * - 1 2 1 1 0
+         */
 
+        // 바구니 N개, M번 공 넣는 횟수
+        string[] NM = Console.ReadLine().Split();
+
+        int N = int.Parse(NM[0]);
+
+        int[] NArray = new int[N + 1];
+
+        int M = int.Parse(NM[1]);
+
+        
+        for (int q = 0; q < M; q++)
+        {
+            // IJK 입력 받기
+            string[] input = Console.ReadLine().Split();
+
+            int i;
+            int j = int.Parse(input[1]);
+
+            int k = int.Parse(input[2]);
+
+            for (i = int.Parse(input[0]); i <= j; i++)
+            {
+                NArray[i] = k;
+            }
+        }
+
+        for (int q = 1; q <= N; q++)
+        {
+            Console.Write(NArray[q] + " ");
+        }
     }
 }
--- BaekJoon/BaekJoon10813/Program.cs
+++ BaekJoon/BaekJoon10813/Program.cs
@@ -5,6 +5,43 @@ public class Program
 {
     private static void Main(string[] args)
     {
+        string[] InputNM = Console.ReadLine().Split();
 
+        // 바구니 N개
+        int N = int.Parse(InputNM[0]);
+        // M번 공 바꾸기
+        int M = int.Parse(InputNM[1]);
+        // 바구니 N개 배열
+        int[] NArray = new int[N + 1];
+        // 저장 공간
+        int Temp;
+
+
+        // 바구니 번호와 같은 번호 공 넣기
+        NArray[0] = 0;
+        for (int i = 1; i <= N; i++)
+        {
+            NArray[i] = i;
+        }
+
+        // M번 바꿈
+        for(int w =0; w < M; w++)
+        {
+            string[] InputIJ = Console.ReadLine().Split();
+
+            int i = int.Parse(InputIJ[0]);
+            int j = int.Parse(InputIJ[1]);
+
+            // 공 교환
+            Temp = NArray[i];
+            NArray[i] = NArray[j];
+            NArray[j] = Temp;
+        }
+
+        // 출력
+        for(int w =1; w <= N; w++)
+        {
+            Console.Write(NArray[w] + " ");
+        }
     }
 }
