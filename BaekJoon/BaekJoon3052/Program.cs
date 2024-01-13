abe33c8f78efd04cb0dbd7806efe0af35428fa61
LAVINSE <freedomsewon@gmail.com>
2024-01-13 18:38:38 +0900

    add
    add

--- BaekJoon/BaekJoon3052/Program.cs
+++ BaekJoon/BaekJoon3052/Program.cs
@@ -5,6 +5,17 @@ public class Program
 {
     private static void Main(string[] args)
     {
-        
+        int b = 42;
+        int[] remain = new int[10];
+
+        // 계산
+        for(int i = 0; i < 10; i++)
+        {
+            int a = int.Parse(Console.ReadLine());
+            remain[i] = a % b;
+        }
+            
+        // 중복 제거
+        Console.WriteLine(remain.Distinct().Count());
     }
 }
