af2aa8bd8ab1c28400ca1fb2bc54b0fad1063c0b
LAVINSE <freedomsewon@gmail.com>
2024-01-18 23:53:32 +0900

    add
    add

---
+++ BaekJoon/BaekJoon9086/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        int t = int.Parse(Console.ReadLine());
+        string[] input = new string[t];
+        char[] last = new char[t];
+        char[] first = new char[t];
+
+        for (int i = 0; i < t; i++)
+        {
+            input[i] = Console.ReadLine();
+            last[i] = input[i].FirstOrDefault();
+            first[i] = input[i][input[i].Length - 1];
+        }
+
+
+        for(int i =0; i < input.Length; i++)
+        {
+            Console.WriteLine($"{last[i]}{first[i]}");
+        } 
+    }
+}
