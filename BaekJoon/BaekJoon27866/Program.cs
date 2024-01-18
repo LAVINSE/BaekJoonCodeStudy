044f02185c4ffc67c36124779eeac22e614977ef
LAVINSE <freedomsewon@gmail.com>
2024-01-18 12:22:59 +0900

    add
    add

---
+++ BaekJoon/BaekJoon27866/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+
+        string inputI = Console.ReadLine();
+        int i = int.Parse(inputI);
+
+        // ElementAt - index를 이용해 값 얻기
+        Console.WriteLine(input.ElementAt(i - 1));
+    }
+}
