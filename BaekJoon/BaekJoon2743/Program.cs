59b32d0c096ddcc68c79efe1b454ba9d8ecc6953
LAVINSE <freedomsewon@gmail.com>
2024-01-18 12:51:03 +0900

    add
    add

---
+++ BaekJoon/BaekJoon2743/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Text;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        Console.WriteLine(input.Length);
+    }
+}
