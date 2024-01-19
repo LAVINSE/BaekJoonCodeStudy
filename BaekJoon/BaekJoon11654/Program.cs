5f9e7cabe9a32f3e70ebaf172a24e0eaf221c2e4
LAVINSE <freedomsewon@gmail.com>
2024-01-19 15:19:33 +0900

    add
    add

---
+++ BaekJoon/BaekJoon11654/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        char inputC = input[0];
+        int ascCode = Convert.ToInt32(inputC);
+        Console.WriteLine(ascCode);
+    }
+}
---
+++ BaekJoon/BaekJoon11720/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        string input = Console.ReadLine();
+        int sum = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            // 문자열로 바꿔서 계산
+            sum += int.Parse(input[i].ToString());
+        }
+
+        // 출력
+        Console.WriteLine(sum);
+    }
+}
