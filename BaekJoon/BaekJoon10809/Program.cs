43f345b30c7c31b9eb070e145d5ffd20e2cf22f7
LAVINSE <freedomsewon@gmail.com>
2024-01-19 22:06:44 +0900

    add
    add

---
+++ BaekJoon/BaekJoon10809/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+
+        for(char i = 'a'; i <= 'z'; i++)
+        {
+            // 같은지 확인
+            if (input.Contains(i))
+            {
+                // 몇번째 위치에 있는지 출력
+                Console.Write(input.IndexOf(Convert.ToChar(i)));
+                Console.Write(" ");
+            }
+            else
+            {
+                // 포함되어 있지 않다면 출력
+                Console.Write("-1 ");
+            }
+        }
+    }
+}
