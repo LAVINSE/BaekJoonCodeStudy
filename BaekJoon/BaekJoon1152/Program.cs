83f3ce6e44cdfbf457ac0c429ae601397330309b
LAVINSE <freedomsewon@gmail.com>
2024-01-21 19:13:33 +0900

    add
    add

---
+++ BaekJoon/BaekJoon1152/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        // 공백기준으로 문자열 분리
+        string[] input = Console.ReadLine().Split();
+        int count = 0;
+
+        // 공백으로 시작하거나 끝날 경우 예외처리
+        for(int i = 0; i < input.Length; i++)
+        {
+            if(input[i] == "")
+            {
+                // 공백 수
+                count++;
+            }
+        }
+
+        // 출력
+        Console.WriteLine(input.Length - count);
+    }
+}
---
+++ BaekJoon/BaekJoon2908/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split();
+        char[] aArray = input[0].ToCharArray();
+        char[] bArray = input[1].ToCharArray();
+        int a;
+        int b;
+
+        // 뒤집기
+        Array.Reverse(aArray);
+        Array.Reverse(bArray);
+
+        // 정수로 변환
+        a = int.Parse(aArray);
+        b = int.Parse(bArray);
+
+        // 출력
+        if(a > b)
+        {
+            Console.WriteLine(a);
+        }
+        else
+        {
+            Console.WriteLine(b);
+        }
+    }
+}
