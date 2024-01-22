bcd58adea5443cc01659c0650943b0d4369ad3a0
LAVINSE <freedomsewon@gmail.com>
2024-01-22 11:50:35 +0900

    add
    add

---
+++ BaekJoon/BaekJoon11718/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        while (true)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(input);
+
+            // null 인지 공백인지 확인
+            if (string.IsNullOrEmpty(input)) return;
+        }
+    }
+}
---
+++ BaekJoon/BaekJoon5622/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        int time = 0;
+
+        for(int i = 0; i < input.Length; i++)
+        {
+            switch(input[i])
+            {
+                case <= 'C':
+                    time += 3;
+                    break;
+                case <= 'F':
+                    time += 4;
+                    break;
+                case <= 'I':
+                    time += 5;
+                    break;
+                case <= 'L':
+                    time += 6;
+                    break;
+                case <= 'O':
+                    time += 7;
+                    break;
+                case <= 'S':
+                    time += 8;
+                    break;
+                case <= 'V':
+                    time += 9;
+                    break;
+                case <= 'Z':
+                    time += 10;
+                    break;
+            }
+        }
+
+        Console.WriteLine(time);
+    }
+}
