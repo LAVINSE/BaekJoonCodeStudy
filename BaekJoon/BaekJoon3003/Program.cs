c5653b231793b41a6a87a13084ba278361bc3d6d
LAVINSE <freedomsewon@gmail.com>
2024-01-23 19:02:32 +0900

    add
    add

---
+++ BaekJoon/BaekJoon3003/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Text;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split();
+        int[] needPiece = { 1, 1, 2, 2, 2, 8 };
+        int[] piece = Array.ConvertAll(input, int.Parse);
+        int[] sum = new int[piece.Length];
+
+        for(int i = 0; i < piece.Length; i++)
+        {
+            // 계산
+            sum[i] = needPiece[i] - piece[i];
+
+            // 출력
+            Console.Write(sum[i] + " ");
+        }
+    }
+}
