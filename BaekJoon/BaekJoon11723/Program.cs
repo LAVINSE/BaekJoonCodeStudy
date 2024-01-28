542c43ffd7cf589802dc2a89b1f9e6d3c5927bd9
LAVINSE <freedomsewon@gmail.com>
2024-01-28 19:16:49 +0900

    fix
    fix

--- BaekJoon/BaekJoon11723/Program.cs
+++ BaekJoon/BaekJoon11723/Program.cs
@@ -6,12 +6,41 @@ public class Program
 {
     private static void Main(string[] args)
     {
-        // https://girawhale.tistory.com/30
+        // 문제 참고 자료 https://www.youtube.com/watch?v=dTnuHZYgpaI&list=PLlf776vnQs4iyN3dtwKzZwLIpf7JXt9Ev
+        // 시간초과 해결해야됨
         int m = int.Parse(Console.ReadLine());
+        int all = (1 << 21) - 1;
+        int answer = 0;
 
-        for(int i = 0; i < m; i++)
+        for (int i = 0; i < m; i++)
         {
             string[] input = Console.ReadLine().Split();
+            int x = int.Parse(input[1]);
+
+            if (input[0] == "all")
+            {
+                answer = all;
+            }
+            else if (input[0] == "Empty")
+            {
+                answer = 0;
+            }
+            else if (input[0] == "add")
+            {
+                answer |= (1 << x);
+            }
+            else if (input[0] == "remove")
+            {
+                answer &= ~(1 << x);
+            }
+            else if (input[0] == "check")
+            {
+                Console.WriteLine(answer = (answer & (1 << x)) != 0 ? 1 : 0);
+            }
+            else if (input[0] == "toggle")
+            {
+                answer ^= (1 << x);
+            }
         }
     }
 }
