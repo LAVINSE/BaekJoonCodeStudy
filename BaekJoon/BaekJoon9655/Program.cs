9775894d1fa2dd6a0eaa58e1b99821c3e048f683
LAVINSE <freedomsewon@gmail.com>
2024-01-29 22:43:13 +0900

    add
    add

--- BaekJoon/BaekJoon9655/Program.cs
+++ BaekJoon/BaekJoon9655/Program.cs
@@ -6,6 +6,19 @@ public class Program
 {
     private static void Main(string[] args)
     {
-        
+        int n = int.Parse(Console.ReadLine());
+
+        // 상근이부터 턴 시작
+        // 홀수, 짝수 판별
+        if(n % 2 == 1)
+        {
+            // 홀수 승리
+            Console.WriteLine("SK");
+        }
+        else
+        {
+            // 짝수 승리
+            Console.WriteLine("CY");
+        }
     }
 }
