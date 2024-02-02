19d755aa5596ce34fe6647e80cd1618b2ef17064
LAVINSE <freedomsewon@gmail.com>
2024-02-02 17:55:18 +0900

    add
    add

---
+++ BaekJoon/BaekJoon7568/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] weight = new int[n];
+        int[] height = new int[n];
+        int count = 0;
+
+        // 입력
+        for(int i = 0; i < n; i++)
+        {
+            int[] input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+            weight[i] = input[0];
+            height[i] = input[1];
+        }
+
+        for(int i =0; i < n; i++)
+        {
+            count = 0;
+
+            for(int j = 0; j < n; j++)
+            {
+                if(height[i] < height[j] && weight[i] < weight[j])
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine(count + 1);
+        }
+    }
+}
