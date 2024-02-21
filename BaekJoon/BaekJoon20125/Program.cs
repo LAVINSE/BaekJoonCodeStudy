3c90e6060e204492389ce2b523137750728fd0fc
LAVINSE <freedomsewon@gmail.com>
2024-02-21 13:16:03 +0900

    fix
    fix

--- BaekJoon/BaekJoon20125/Program.cs
+++ BaekJoon/BaekJoon20125/Program.cs
@@ -4,8 +4,10 @@ using System.Text;
 
 public class Program
 {
+    // 심장 위치를 찾는다
     private static void FindHeart(string[] array, int n, out int x, out int y)
     {
+        // 초기화
         x = y = -1;
 
         for (int i = 0; i < n; i++)
@@ -32,15 +34,17 @@ public class Program
         int subX = 0;
         int subY = 0;
 
+        // 문자 입력
         for (int i = 0; i < n; i++)
             array[i] = Console.ReadLine();
 
-        
+        // 심장 위치 찾기
         FindHeart(array, n, out x, out y);
 
+        // 심장 위치 출력
         Console.WriteLine($"{x + 1} {y + 1}");
 
-        
+        // 왼쪽으로 이동 하면서 왼쪽 팔 길이 체크
         for (int i = 1; y - i >= 0; i++)
         {
             if (array[x][y - i] != '*')
@@ -48,9 +52,11 @@ public class Program
             answer++;
         }
 
+        // 출력, 초기화
         Console.Write($"{answer} ");
         answer = 0;
 
+        // 오른쪽으로 이동 하면서 오른쪽 팔 길이 체크
         for (int i = 1; y + i < n; i++)
         {
             if (array[x][y + i] != '*')
@@ -58,11 +64,14 @@ public class Program
             answer++;
         }
 
+        // 출력, 초기화
         Console.Write($"{answer} ");
         answer = 0;
-        
+
+        // 아래로 이동하면서 허리 길이 체크
         for (int i = 1; x + i < n; i++)
         {
+            // 허리가 끝나는 위치 저장
             if (array[x + i][y] != '*')
             {
                 subX = x + i - 1;
@@ -72,9 +81,11 @@ public class Program
             answer++;
         }
 
+        // 출력, 초기화
         Console.Write($"{answer} ");
         answer = 0;
 
+        // 왼쪽 대각선으로 내려가면서 다리 길이 체크
         for (int i = 1; subX + i < n; i++)
         {
             if (array[subX + i][subY - 1] != '*')
@@ -82,9 +93,11 @@ public class Program
             answer++;
         }
 
+        // 출력, 초기화
         Console.Write($"{answer} ");
         answer = 0;
 
+        // 오른쪽 대각선으로 내려가면서 다리 길이 체크
         for (int i = 1; subX + i < n; i++)
         {
             if (array[subX + i][subY + 1] != '*')
@@ -92,6 +105,7 @@ public class Program
             answer++;
         }
 
+        // 출력
         Console.Write($"{answer} ");
     }
 }
