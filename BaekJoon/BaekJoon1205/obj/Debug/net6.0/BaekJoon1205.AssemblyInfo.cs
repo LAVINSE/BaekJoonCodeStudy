974add8afd613deddfeab6a76182536cd6b30df3
오세원 (Oh SeWon) <freedomsewon@gmail.com>
2024-08-05 09:39:29 +0900

    20920

--- BaekJoon/BaekJoon1205/obj/Debug/net6.0/BaekJoon1205.AssemblyInfo.cs
+++ BaekJoon/BaekJoon1205/obj/Debug/net6.0/BaekJoon1205.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BaekJoon1205")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b5f77f058ed24b26822d0147988e66efbdd8699")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fb6c6e0cec8d8b5cc3f0a091adb5f9c923cf5d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("BaekJoon1205")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BaekJoon1205")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ BaekJoon/BaekJoon20920/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        // 자주 나오는 단어 앞에
+        // 해당 단어 길이가 길면 앞에
+        // 사전순으로 앞에
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        int[] input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+        int n = input[0]; // 단어 개수
+        int m = input[1]; // 단어의 길이 기준
+
+        Dictionary<string, int> wordDict = new Dictionary<string, int>();
+
+        for(int i = 0; i < n; i++)
+        {
+            string word = Console.ReadLine();
+
+            if (word.Length < m)
+                continue;
+
+            if (wordDict.ContainsKey(word))
+                wordDict[word]++;
+            else
+                wordDict[word] = 1;
+        }
+
+        // 자주 나오는 단어, 단어 길이가 길고, 사전순으로 정렬 > string으로 리스트 추출
+        List<string> wordList = wordDict.OrderByDescending(x => x.Value).ThenByDescending(x => x.Key.Length).ThenBy(x => x.Key).Select(x => x.Key).ToList();
+
+        foreach(string word in wordList)
+        {
+            stringBuilder.AppendLine(word);
+        }
+
+        Console.Write(stringBuilder);
+    }
+}
