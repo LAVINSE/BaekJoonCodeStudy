1c0c79c0dcf570a643f35bbfedab134bded06090
오세원 (Oh SeWon) <freedomsewon@gmail.com>
2024-08-14 09:28:19 +0900

    2607

--- BaekJoon/BaekJoon2607/Program.cs
+++ BaekJoon/BaekJoon2607/Program.cs
@@ -6,6 +6,37 @@ public class Program
 {
     private static void Main(string[] args)
     {
-        
+        // 단어의 개수 N
+        // 개수만큼 단어 입력
+
+        int n = int.Parse(Console.ReadLine());
+        List<char> target = new List<char>(Console.ReadLine());
+        int answer = 0;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            List<char> compare = new List<char>(target);
+            string word = Console.ReadLine();
+            int cnt = 0;
+
+            foreach (char w in word)
+            {
+                if (compare.Contains(w))
+                {
+                    compare.Remove(w);
+                }
+                else
+                {
+                    cnt++;
+                }
+            }
+
+            if (cnt < 2 && compare.Count < 2)
+            {
+                answer++;
+            }
+        }
+
+        Console.WriteLine(answer);
     }
 }
--- BaekJoon/BaekJoon2743/obj/Debug/net6.0/BaekJoon2743.AssemblyInfo.cs
+++ BaekJoon/BaekJoon2743/obj/Debug/net6.0/BaekJoon2743.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BaekJoon2743")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bf93d4c36d7fb3b02861cda999d54a2dbb7b130")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ee05f3c7ba50145e1d602a12a292a504d66da7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BaekJoon2743")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BaekJoon2743")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
